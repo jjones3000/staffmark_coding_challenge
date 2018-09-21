f4468184906540ca25b191fce10e19153283b3e4
jjones3000 <jeff@gridlevelwd.com>
2018-09-21 12:13:14 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,53 +1,19 @@
 <?php
-/**
- * Login page. This provides authentication against mysqli database 
- * using sha1 encrypted password, then redirects to the fileUpload.php page
- *
- */
-session_start();
-include ('includes/fxns.inc.php');
-include ('includes/db.inc.php');
-$error = $user = $pass ='';
 
-	if (isset($_POST['user']))
-	{
-		$user=sanitizeString($_POST['user']);
-		$pass = sanitizeString($_POST['pass']);
-		$pass=sha1($pass);
 
-		if ($user == "" || $pass == "")
-		{
-			$error = "Not all fields were entered<br />";
-		}
-		else
-		{
-			$query = "SELECT * FROM login WHERE username='".$user."' AND password='".$pass."'";
-			$result=$conn->query($query);
-			$numMatches=$result->num_rows;
-				if ($numMatches == 0)
-				{
-					$error = "Username/Password invalid<br />";
-				}
-				else
-				{
-					$_SESSION['user'] = $user;
-					$_SESSION['pass'] = $pass;
-					die(header('location:./fileUpload.php'));
-				}
-			
-		}
-	}
-
-echo <<<_END
-<html>
-<head><title></title></head>
-<body>
-	<form method='POST' action='index.php'>$error
-	Username <input type='text' maxlength='16' name='user' value='$user' /><br />
-	Password <input type='password' maxlength='16' name='pass' /><br />
-	<input type='submit' value='Login' />
-	</form>
-</body>
-</html>
-_END
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head><title>Exten Excellent</title></head>
+<h1>Hi Brian</h1>
+<h2>this is a little proof of concept</h2>
+
+<h3>check it out</h3>
+<h2>These go to eleven!!!</h2>
+
+<br>
+
+<br>
+
+
+</html>
